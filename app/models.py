5d8b5d7c79cb62e5ee034bc0dc336151efae4914
Div-30 <divinpaul88@gmail.com>
2026-05-11 14:22:32 +0200

    feat: add database models

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from typing import Optional
+from typing import Any, Optional, Dict
 
-from sqlmodel import TIMESTAMP, Column, Field, SQLModel, text
+from sqlmodel import TIMESTAMP, Column, Field, Relationship, SQLModel, text
+from sqlalchemy.dialects.postgresql import JSONB
 
 
 class User(SQLModel, table=True):
@@ -13,4 +14,28 @@ class User(SQLModel, table=True):
     hashed_password: str
     is_active: bool = Field(default=True)
     created_at: Optional[datetime] = Field(default=None, sa_column=Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
-    
+    trips: list["Trip"] = Relationship(back_populates="owner")
+
+class Trip(SQLModel, table=True):
+    __tablename__ = "trips"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    destination: str = Field(max_length=50)
+    days: int = Field(gt=0)
+    budget: int = Field(gt=0)
+    trip_style: str = Field(max_length=100)
+    owner_id: Optional[int] = Field(default=None, foreign_key="users.id", ondelete="CASCADE")
+    created_at: Optional[datetime] = Field(default=None, sa_column=Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
+    owner: Optional["User"] = Relationship(back_populates="trips")
+    itinerary: Optional["Itinerary"] = Relationship(back_populates="trip")
+
+class Itinerary(SQLModel, table=True):
+    __tablename__ = "itineraries"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    trip_id: Optional[int] = Field(default=None, foreign_key="trips.id", unique=True, ondelete="CASCADE")
+    days: list[dict[str, Any]] = Field(default_factory=list, sa_column=Column(JSONB))
+    created_at: Optional[datetime] = Field(default=None, sa_column=Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP')))
+    trip: Optional["Trip"] = Relationship(back_populates="itinerary")
+
+
