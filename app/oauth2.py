0dc06744edf7c99e888b4c5bb35927b977481152
Div-30 <divinpaul88@gmail.com>
2026-05-11 18:27:06 +0200

    feat: add oauth2 file

---
+++ app/oauth2.py
@@ -0,0 +1,21 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session
+
+from app import models
+from app.database import get_db
+from app.utils import verify_access_token
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> models.User:
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    token_data = verify_access_token(token, credentials_exception)
+    user = user_service.get_user_by_id(db, token_data.id)
+    if not user:
+        raise credentials_exception
+    if not user.is_active:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Account is disabled")
+    return user
+    
