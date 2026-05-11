119a89ffa980d3c6e558a4b0b29adb4d4ee375f4
Div-30 <divinpaul88@gmail.com>
2026-05-11 18:55:38 +0200

    feat: add auth_service file

---
+++ app/services/auth_service.py
@@ -0,0 +1,17 @@
+from sqlmodel import Session
+
+from app import models
+from app.services import user_service
+from app.utils import verify_password
+
+
+def authenticate_user(db: Session, username: str, password: str) -> models.User | None:
+    user = user_service.get_user_by_name(db, username)
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    if not user.is_active:
+        return None
+    return user
+    
