138184be5e50599b4980a71f8be41d3b14bed923
Div-30 <divinpaul88@gmail.com>
2026-05-12 11:42:15 +0200

    feat: added the user router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from app.routers import auth, itineraries, trips
+from app.routers import auth, itineraries, trips, users
 
 
 app = FastAPI(title="AI_Vacation_Planner")
 
 app.include_router(auth.router)
+app.include_router(users.router)
 app.include_router(trips.router)
 app.include_router(itineraries.router)
